d414db62bad940ba5408114a973ee14041320f09
adiMallya <adimallya99@gmail.com>
2022-01-18 01:30:15 +0530

    refactor: theme, splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:forum_app/screens/splash_screen.dart';
 import 'package:forum_app/services/auth.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
-import 'package:forum_app/screens/wrapper.dart';
+import 'package:google_fonts/google_fonts.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -25,9 +26,15 @@ class MyApp extends StatelessWidget {
           initialData: null,
         ),
       ],
-      child: const MaterialApp(
+      child: MaterialApp(
+        home: const SplashScreen(),
+        theme: ThemeData(
+          primarySwatch: Colors.cyan,
+          textTheme: GoogleFonts.poppinsTextTheme(
+            Theme.of(context).textTheme,
+          ),
+        ),
         debugShowCheckedModeBanner: false,
-        home: Wrapper(),
       ),
     );
   }
