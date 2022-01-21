b0f47205b1e355314d4301cb0a412d15b0a97f28
adiMallya <adimallya99@gmail.com>
2022-01-21 20:19:19 +0530

    chore: init user data

---
+++ lib/screens/home.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:forum_app/models/user.dart';
+import 'package:forum_app/screens/feed/my_feed.dart';
+import 'package:forum_app/screens/profile.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Home extends StatefulWidget { 
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home>{
+  
+  final PageController _pageController = PageController();
+  late UserData myData;
+
+  int pageIndex = 0;
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _getMyData();
+  }
+
+  Future<void> _getMyData() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      myData = UserData(
+        name: prefs.getString('myName'),
+        email: prefs.getString('myEmail'),
+        myVoteList: prefs.getStringList('myVoteList'),
+        myVoteAnswerList: prefs.getStringList('myVoteAnswerList')
+      );
+    });
+
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  void updateMyData(UserData newMyData) {
+    setState(() {
+      myData = newMyData;
+    });
+  }
+
+  void onTabTapped(int index) {
+    setState(() {
+      pageIndex = index;
+    });
+    _pageController.jumpToPage(index);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: isLoading
+        ? Center(
+            child: Container(
+                height: size.height / 20,
+                width: size.height / 20,
+                child: const CircularProgressIndicator(),
+              ),
+          )
+        : PageView(
+            controller: _pageController,
+            children: [
+              const MyFeed(), 
+              Profile(myData: myData, updateMyData: updateMyData)
+            ],
+            physics: const NeverScrollableScrollPhysics(),
+            onPageChanged: (page) {
+              pageIndex = page;
+            },
+          ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: onTabTapped,
+        currentIndex: pageIndex,
+        unselectedItemColor: Colors.grey[400],
+        showSelectedLabels: true,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_filled),
+            label: "Home"
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: "Profile"
+          ),
+        ],
+      ),
+    );
+  }
+}
