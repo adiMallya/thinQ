c3146a9429fd53b44da3c5c22f7be4d3d45f0fff
adiMallya <adimallya99@gmail.com>
2022-01-26 03:36:59 +0530

    refactor: splash text

--- lib/views/screens/splash_screen.dart
+++ lib/views/screens/splash_screen.dart
@@ -17,7 +17,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     Timer(
       const Duration(
-        seconds: 1
+        seconds: 2
       ), 
       () => Navigator.push(
         context, 
@@ -31,22 +31,23 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.cyan,
       body: Center(
         child: RichText(
           text: const TextSpan(
-            text: 'the',
+            text: 'THIN',
             style: TextStyle(
-              color: Colors.grey,
+              color: Colors.white,
               fontWeight: FontWeight.bold,
-              fontSize: 30.0,
+              fontSize: 35.0,
             ),
             children: <TextSpan>[
               TextSpan(
-                text: "Forum",
+                text: "Q",
                 style: TextStyle(
-                  color: Colors.cyan,
+                  color: Colors.white,
                   fontWeight: FontWeight.bold,
-                  fontSize: 34.0,
+                  fontSize: 50.0,
                 ),
               )
             ]
